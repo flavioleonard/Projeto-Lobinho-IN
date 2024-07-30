027b843b765bb5622ef2a0276428d3ee194753a8
João Vicente Piller Menezes <joaovicentepm14@gmail.com>
2024-07-29 21:58:04 -0300

    feat(show-lobinho): implementei a função deletar

--- show-lobinho/script.js
+++ show-lobinho/script.js
@@ -1,26 +1,42 @@
+//PERSONALIZAR PÁGINA DE ACORDO COM ID DO LOBO
+
 function obterParametroURL(nome) {
     const url = new URL(window.location.href);
     return url.searchParams.get(nome);
 }
 
-function obterDetalhesLobo(index) {
-    let lobos = JSON.parse(localStorage.getItem('lobos'))
-    return lobos[index-1]
+function obterDetalhesLobo(id) {
+    let lobos = JSON.parse(localStorage.getItem('lobos'));
+    return lobos[id-1];
 }
 
-const loboId = obterParametroURL('index')
-const detalhesLobo = obterDetalhesLobo(loboId)
+const loboId = obterParametroURL('id');
+const detalhesLobo = obterDetalhesLobo(loboId);
 
-const showLobinho = document.querySelector('.main')
+const showLobinho = document.querySelector('.main');
 showLobinho.innerHTML = `
 <h1>${detalhesLobo.nome}</h1>
 <section class="show-lobinho">
     <div class="secao-esquerda">
         <img src="${detalhesLobo.imagem}" alt="">
         <div class="botoes">
-            <button class="adotar">ADOTAR</button>
+            <button class="adotar" onclick="window.location.href='/adotar-lobinho';">ADOTAR</button>
             <button class="excluir">EXCLUIR</button>
         </div>
     </div>
     <p>${detalhesLobo.descricao}</p>
 </section>`;
+
+
+//DELETANDO LOBO DA LISTA
+
+function deletarLobo(id) {
+    let lobos = JSON.parse(localStorage.getItem('lobos'));
+    lobos = lobos.filter((lobo, index) => index !== id-1);
+    localStorage.setItem('lobos', JSON.stringify(lobos));
+    alert('Lobo excluido com sucesso!');
+    window.location.href = '/lista-de-lobinhos';
+}
+
+let botaoExcluir = document.querySelector('.excluir')
+botaoExcluir.addEventListener('click', () => deletarLobo(loboId));
