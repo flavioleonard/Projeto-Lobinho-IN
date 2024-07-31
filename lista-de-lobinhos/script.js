2082b9832499da84c194dddc31f03ef2053337dd
João Vicente Piller Menezes <joaovicentepm14@gmail.com>
2024-07-30 22:33:01 -0300

    feat: filtrar por adotados na lista de lobinhos

--- lista-de-lobinhos/script.js
+++ lista-de-lobinhos/script.js
@@ -1,105 +1,64 @@
 const itensPorPagina = 4;
 let paginaAtual = 1;
+let mostrarApenasAdotados = false;
 
 function lobosNaoAdotados() {
     let lobos = JSON.parse(localStorage.getItem('lobos'));
     let lobosNaoAdotados = lobos.filter(lobo => lobo.adotado === false);
-    return lobosNaoAdotados
+    return lobosNaoAdotados;
+}
+
+function lobosAdotados() {
+    let lobos = JSON.parse(localStorage.getItem('lobos'));
+    let lobosAdotados = lobos.filter(lobo => lobo.adotado === true);
+    return lobosAdotados;
+}
+
+function obterLobos() {
+    return mostrarApenasAdotados ? lobosAdotados() : lobosNaoAdotados();
 }
 
 function exibirItens() {
     const container = document.querySelector('.show-lobos');
     container.innerHTML = '';
 
-    const lobos = lobosNaoAdotados();
+    const lobos = obterLobos();
     const inicio = (paginaAtual - 1) * itensPorPagina;
     const fim = inicio + itensPorPagina;
     const itensPagina = lobos.slice(inicio, fim);
 
-    container.innerHTML = `
-                    <div class="lobo lobo-esquerda">
-                    <div class="secao-1">
-                        <img class="icone-lobo" src="${itensPagina[0].imagem}">
+    itensPagina.forEach((lobo, index) => {
+        const classeBotao = mostrarApenasAdotados ? 'btn-adotado' : 'btn-adotar';
+        const textoBotao = mostrarApenasAdotados ? 'Adotado' : 'Adotar';
+
+        container.innerHTML += `
+            <div class="lobo ${index % 2 === 0 ? 'lobo-esquerda' : 'lobo-direita'}">
+                <div class="secao-1">
+                    <img class="icone-lobo" src="${lobo.imagem}">
+                </div>
+                <div class="secao-2">
+                    <div class="secao-2-1">
+                        <div class="secao-2-1-1">
+                            <h2>${lobo.nome}</h2>
+                            <p>Idade: ${lobo.idade} anos</p>
+                        </div>
+                        <div class="secao-2-1-2">
+                            <button class="${classeBotao}">${textoBotao}</button>
+                        </div>
                     </div>
-                    <div class="secao-2">
-                        <div class="secao-2-1">
-                            <div class="secao-2-1-1">
-                                <h2>${itensPagina[0].nome}</h2>
-                                <p>Idade: ${itensPagina[0].idade} anos</p>
-                            </div>
-                            <div class="secao-2-1-2">
-                                <button>Adotar</button>
-                            </div>
-                        </div>
-                        <div class="secao-2-2">
-                            <p>${itensPagina[0].descricao}</p>
-                        </div>
+                    <div class="secao-2-2">
+                        <p>${lobo.descricao}</p>
                     </div>
                 </div>
-                <div class="lobo lobo-direita">
-                    <div class="secao-1">
-                        <img class="icone-lobo" src="${itensPagina[1].imagem}">
-                    </div>
-                    <div class="secao-2">
-                        <div class="secao-2-1">
-                            <div class="secao-2-1-1">
-                                <h2>${itensPagina[1].nome}</h2>
-                                <p>Idade: ${itensPagina[1].idade} anos</p>
-                            </div>
-                            <div class="secao-2-1-2">
-                                <button>Adotar</button>
-                            </div>
-                        </div>
-                        <div class="secao-2-2">
-                            <p>${itensPagina[1].descricao}</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="lobo lobo-esquerda">
-                    <div class="secao-1">
-                        <img class="icone-lobo" src="${itensPagina[2].imagem}">
-                    </div>
-                    <div class="secao-2">
-                        <div class="secao-2-1">
-                            <div class="secao-2-1-1">
-                                <h2>${itensPagina[2].nome}</h2>
-                                <p>Idade: ${itensPagina[2].idade} anos</p>
-                            </div>
-                            <div class="secao-2-1-2">
-                                <button>Adotar</button>
-                            </div>
-                        </div>
-                        <div class="secao-2-2">
-                            <p>${itensPagina[2].descricao}</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="lobo lobo-direita">
-                    <div class="secao-1">
-                        <img class="icone-lobo" src="${itensPagina[3].imagem}">
-                    </div>
-                    <div class="secao-2">
-                        <div class="secao-2-1">
-                            <div class="secao-2-1-1">
-                                <h2>${itensPagina[3].nome}</h2>
-                                <p>Idade: ${itensPagina[3].idade} anos</p>
-                            </div>
-                            <div class="secao-2-1-2">
-                                <button>Adotar</button>
-                            </div>
-                        </div>
-                        <div class="secao-2-2">
-                            <p>${itensPagina[3].descricao}</p>
-                        </div>
-                    </div>
-                </div>`;
+            </div>`;
+    });
 }
 
 function exibirPaginacao() {
     const controle = document.getElementById('controle-paginacao');
     controle.innerHTML = '';
 
-    const lobos = lobosNaoAdotados();
+    const lobos = obterLobos();
     const totalPaginas = Math.ceil(lobos.length / itensPorPagina);
 
     if (paginaAtual > 1) {
@@ -125,37 +84,51 @@ function exibirPaginacao() {
     }
 }
 
-
 function filtrarLobos() {
-    let lobos = lobosNaoAdotados()
-    const inputPesquisa = document.querySelector('.procurar-lobo').value.toLowerCase()
-    let loboFiltrado = lobos.filter((lobo) => lobo.nome.toLowerCase() === inputPesquisa)
+    const lobos = obterLobos();
+    const inputPesquisa = document.querySelector('.procurar-lobo').value.toLowerCase();
+    const loboFiltrado = lobos.filter((lobo) => lobo.nome.toLowerCase() === inputPesquisa);
 
     const container = document.querySelector('.show-lobos');
-    container.innerHTML = `<div class="lobo lobo-esquerda">
-                    <div class="secao-1">
-                        <img class="icone-lobo" src="${loboFiltrado[0].imagem}">
-                    </div>
-                    <div class="secao-2">
-                        <div class="secao-2-1">
-                            <div class="secao-2-1-1">
-                                <h2>${loboFiltrado[0].nome}</h2>
-                                <p>Idade: ${loboFiltrado[0].idade} anos</p>
-                            </div>
-                            <div class="secao-2-1-2">
-                                <button>Adotar</button>
-                            </div>
+    container.innerHTML = '';
+
+    loboFiltrado.forEach((lobo, index) => {
+        const classeBotao = mostrarApenasAdotados ? 'btn-adotado' : 'btn-adotar';
+        const textoBotao = mostrarApenasAdotados ? 'Adotado' : 'Adotar';
+
+        container.innerHTML += `
+            <div class="lobo ${index % 2 === 0 ? 'lobo-esquerda' : 'lobo-direita'}">
+                <div class="secao-1">
+                    <img class="icone-lobo" src="${lobo.imagem}">
+                </div>
+                <div class="secao-2">
+                    <div class="secao-2-1">
+                        <div class="secao-2-1-1">
+                            <h2>${lobo.nome}</h2>
+                            <p>Idade: ${lobo.idade} anos</p>
                         </div>
-                        <div class="secao-2-2">
-                            <p>${loboFiltrado[0].descricao}</p>
+                        <div class="secao-2-1-2">
+                            <button class="${classeBotao}">${textoBotao}</button>
                         </div>
                     </div>
-                </div>`;
+                    <div class="secao-2-2">
+                        <p>${lobo.descricao}</p>
+                    </div>
+                </div>
+            </div>`;
+    });
+
     document.querySelector('.procurar-lobo').value = '';
 }
 
-exibirItens()
-exibirPaginacao()
+document.querySelector('.lobosAdotados').addEventListener('change', (event) => {
+    mostrarApenasAdotados = event.target.checked;
+    paginaAtual = 1;
+    exibirItens();
+    exibirPaginacao();
+});
 
-let botaoProcurar = document.querySelector('.btn-procurar');
-botaoProcurar.addEventListener('click', () => filtrarLobos());
+exibirItens();
+exibirPaginacao();
+
+document.querySelector('.btn-procurar').addEventListener('click', filtrarLobos);
